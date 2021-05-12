a04cab612066418b3de35a000f4d215ba4a092d5
Janeth <60449001+jymeraz@users.noreply.github.com>
2021-05-12 15:17:11 -0600

    Add files via upload

---
+++ Meraz_Janeth_Hw10.R
@@ -0,0 +1,79 @@
+##############
+# By: Janeth Meraz
+# Course: STAT 4329
+# Instructor: Dr. Abhijit Mandal
+##############
+
+##############
+## QUESTION 1
+##############
+
+# Separate file is included.
+
+##############
+## QUESTION 2
+##############
+
+library(ggplot2)
+library(ggpubr)
+
+# Read the data from the file.
+# data <- read.csv("diamond.csv","rt")
+data <- read.csv("/Users/boo/Documents/STAT4329/Hw10/diamond.csv")
+
+# Format the categorical variables.
+terms_cut <- c(data$cut[0])
+for(val in data$cut) {
+  if(!is.element(val, terms_cut)){
+    terms_cut <- c(terms_cut, val)
+  }
+}
+
+terms_color <- c(data$color[0])
+for(val in data$color) {
+  if(!is.element(val, terms_color)){
+    terms_color <- c(terms_color, val)
+  }
+}
+
+data$cut <- factor(data$cut, levels = terms_cut)
+data$color <- factor(data$color, levels = terms_color)
+data$clarity <- factor(data$clarity, levels = c("I1", "SI2", "SI1", "VS2", "VS1", "VVS2", "VVS1", "IF"))
+
+# Plot the data with least-squares regression line and LOESS curve.
+ggplot(data=data, mapping=aes(x=price, y=carat)) +
+  geom_point(mapping=aes(colour=color, shape=cut, alpha=clarity)) +
+  geom_smooth(method=lm) +
+  stat_cor(label.x=5) +
+  geom_smooth(colour="red") +
+  xlab("Diamond Price") +
+  ylab("Diamond Carat") + 
+  theme_bw()
+
+# We can see from the data that as the diamond price increases, the diamond carat increases as well.
+# The cut types seem to be scattered throughout, with all types of cuts being found in every price range, so diamond price may not depend on this variable but this is not definite.
+# The clarity values of "SI1", "SI2", and "I1" seem to have a higher carat and a higher price range than others.
+# This shows that diamond price does depend on clarity.
+# The price of the diamond also depends on color, where color F and E are primarily the cheaper ones and color J tends to be at the pricier range.
+# Since we are seeing three categorical variables in one plot, it's difficult to quickly see relationships, but these capture the overall behavior. 
+
+##############
+## QUESTION 3
+##############
+
+library(magrittr)
+
+# Using pipes (%>%) write the following operation:
+print(exp(round(log(3.4), 2)), 7)
+
+3.4 %>% log() %>%
+  round(2) %>% 
+  exp() %>%
+  print(7)
+
+
+
+
+
+
+
---
+++ app.R
@@ -0,0 +1,159 @@
+##############
+# By: Janeth Meraz
+# Course: STAT 4329
+# Instructor: Dr. Abhijit Mandal
+##############
+
+##############
+## QUESTION 1
+##############
+
+library(shiny)
+
+# Load the data.
+# Factor the categorical variables.
+data <- read.csv("/Users/boo/Documents/STAT4329/Hw10/diamond.csv")
+data_2 <- data
+terms_cut <- c(data$cut[0])
+for(val in data$cut) {
+    if(!is.element(val, terms_cut)){
+        terms_cut <- c(terms_cut, val)
+    }
+}
+terms_cut <- sort(terms_cut)
+
+terms_color <- c(data$color[0])
+for(val in data$color) {
+    if(!is.element(val, terms_color)){
+        terms_color <- c(terms_color, val)
+    }
+}
+terms_color <- sort(terms_color)
+
+term_clarity <- sort(c("I1", "SI2", "SI1", "VS2", "VS1", "VVS2", "VVS1", "IF"))
+
+data_2$cut <- factor(data$cut, levels = terms_cut)
+data_2$color <- factor(data$color, levels = terms_color)
+data_2$clarity <- factor(data$clarity, levels = term_clarity)
+
+
+# Define UI for application that displays descriptive statistics for diamond data.
+ui <- fluidPage(
+    
+    # Application title
+    titlePanel("Descriptive Statistics for the Diamond Data"),
+    br(),
+    br(),
+    
+    
+    # Display the table and number of chosen rows. 
+    sidebarLayout(
+        sidebarPanel(
+            numericInput("rows", label = h4("Choose the number of rows to be shown: "), value = 10)
+        ),
+        
+        # Show a table of the data columns.
+        mainPanel(
+            h3("Data preview:"),
+            DT::dataTableOutput("table")
+        )
+    ),
+    
+    br(),
+    br(),
+    br(),
+    
+    # Descriptive statistics.
+    h2("Diamond prices"),
+    p("Prices of 3,000 round cut diamonds"),
+    br(),
+    
+    h2("Description"),
+    p("A dataset containing the prices and other attributes of a sample of 3000 diamonds. The variables are as follows:"),
+    br(),
+    
+    h2("Variables"),
+    p("- price = price in US dollars ($338–$18,791)"),
+    p("- carat = weight of the diamond (0.2–3.00)"),
+    p("- clarity = a measurement of how clear the diamond is (I1 (worst), SI2, SI1, VS2, VS1, VVS2, VVS1, IF (best))"),
+    p("- cut = quality of the cut (Fair, Good, Very Good, Premium, Ideal)"),
+    p("- color = diamond color, from J (worst) to D (best)"),
+    p("- depth = total depth percentage = z / mean(x, y) = 2 * z / (x + y) (54.2–70.80)"),
+    p("- table = width of top of diamond relative to widest point (50–69)"),
+    p("- x = length in mm (3.73–9.42)"),
+    p("- y = width in mm (3.71–9.29)"),
+    p("- z = depth in mm (2.33–5.58)"),
+    p("- date = shipment date"),
+    br(),
+    
+    h2("Additional information"),
+    tags$a(href="https://www.diamondse.info/diamonds-clarity.asp", "Diamond search engine"),
+    
+    # Vizualizations.
+    # Include a drop menu for each type of table. 
+    h1("Vizualizations"),
+    br(),
+    sidebarLayout(
+        sidebarPanel(
+            selectInput("col_type", label = h3("Select the column:"), 
+                        choices = list("price" = 'price', 
+                                       "carat" = 'carat', 
+                                       "clarity" = 'clarity', 
+                                       "cut" = 'cut',
+                                       "color" = 'color',
+                                       "depth" = 'depth', 
+                                       "table" = 'table', 
+                                       "x" = 'x', 
+                                       "y" = 'y', 
+                                       "z" = 'z', 
+                                       "date" = 'date'), 
+                        selected = 'price'),
+        ),
+        
+        mainPanel(
+            plotOutput("distPlot")
+        )
+    )
+)
+
+# Function to handle all the input/output.
+server <- function(input, output) {
+    output$table <- DT::renderDataTable({
+        if (is.na(input$rows) | input$rows <= 0) {
+            return()
+        }
+        shown <- head(data,input$rows)
+        DT::datatable(shown,options = list(dom = 't'))
+    })
+    
+    output$distPlot <- renderPlot({
+        
+        if(input$col_type == 'price') {
+            hist(x=data$price, xlab="price", main="A histogram of the price of diamond")
+        } else if (input$col_type == 'carat') {
+            hist(x=data$carat, xlab="carat", main="A histogram of the carat of diamond")
+        } else if (input$col_type == 'clarity') {
+            pie(table(factor(data$clarity)),main="A pie diagram of the clarity of diamond")
+        } else if (input$col_type == 'cut') {
+            pie(table(factor(data$cut)),main="A pie diagram of the cut of diamond")
+        } else if (input$col_type == 'color') {
+            pie(table(factor(data$color)),main="A pie diagram of the color of diamond")
+        } else if (input$col_type == 'depth') {
+            hist(x=data$depth, xlab="depth", main="A histogram of the depth of diamond")
+        } else if (input$col_type == 'table') {
+            hist(x=data$table, xlab="table", main="A histogram of the table of diamond")
+        } else if (input$col_type == 'x') {
+            hist(x=data$x, xlab="x", main="A histogram of the x of diamond")
+        } else if (input$col_type == 'y') {
+            hist(x=data$y, xlab="y", main="A histogram of the y of diamond")
+        } else if (input$col_type == 'z') {
+            hist(x=data$z, xlab="z", main="A histogram of the z of diamond")
+        } else if (input$col_type == 'date') {
+            year <- str_sub(data$date,1,4) 
+            pie(table(factor(year)),main="A pie diagram of the date of diamond")
+        }
+    })
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
